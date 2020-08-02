66647e6ee533a63e90bf4bf0d98226d303eb65c6
Fabian-MG <mirandafabian1510@gmail.com>
2020-08-01 19:57:22 -0500

    Configurating heroku for server and database deployment

--- src/index.js
+++ src/index.js
@@ -38,9 +38,10 @@ const batchUsers = async (keys, models) => {
   return keys.map((key) => users.find((user) => user.id === key));
 };
 
-const userLoader = new DataLoader((keys) => batchUsers(keys, models))
+const userLoader = new DataLoader((keys) => batchUsers(keys, models));
 
 const server = new ApolloServer({
+  introspection: true,
   typeDefs: schema,
   resolvers,
   formatError: (error) => {
@@ -77,14 +78,16 @@ server.installSubscriptionHandlers(httpServer);
 // const eraseDatabaseOnSync = true;
 
 const isTest = !!process.env.TEST_DATABASE;
+const isProduction = !!process.env.DATABASE_URL;
+const port = process.env.PORT || 8000;
 
-sequelize.sync({ force: isTest }).then(async () => {
-  if (isTest) {
+sequelize.sync({ force: isTest || isProduction }).then(async () => {
+  if (isTest || isProduction) {
     createUsersWithMessages(new Date());
   }
 
-  httpServer.listen({ port: 8000 }, () => {
-    console.log("Apollo Server on http://localhost:8000/graphql");
+  httpServer.listen({ port }, () => {
+    console.log(`Apollo Server on http://localhost:${port}/graphql`);
   });
 });
 
